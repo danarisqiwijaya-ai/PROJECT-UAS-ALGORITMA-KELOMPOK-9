1a8abae876b01253a37d397b6a56bb25d5eca453
danarisqiwijaya-ai <danarisqiwijaya@gmail.com>
2026-06-02 01:12:03 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,73 +1,94 @@
-frame_button = Frame(root, bg="#101820")
-frame_button.pack()
+from tkinter import *
+from data import club_list, history_stack, watchlist_queue
+from model import Club
+from logic import cari_club, bubble_sort, binary_search
 
-Button(
-       frame_button,
-       text="Tambah Club", 
-       width=18, 
-       command=tambah_club
-).grid(row=0, column=0, padx=5, pady=5)
+root = Tk()
+root.title("Football Recommendation System")
+root.geometry("900x650")
+root.configure(bg="#101820")
 
-Button(frame_button, 
-       text="Update Club", 
-       width=18, 
-       command=update_club
-).grid(row=0, column=1, padx=5, pady=5)
+listbox = Listbox(root, width=100)
+listbox.pack(pady=20)
 
-Button(frame_button,
-       text="Hapus Club",
-       width=18, 
-       command=hapus_club
-).grid(row=0, column=2padx=5, pady=5)
 
-Button(frame_button, 
-       text="Cari Club",
-       width=18, 
-       command=cari_data
-).grid(row=1, column=0, padx=5, pady=5)
+def refresh():
+    listbox.delete(0, END)
+    for c in club_list.tampil_semua():
+        listbox.insert(END, c.tampil())
 
-Button(frame_button, 
-       text="Urutkan Poin",
-       width=18, 
-       command=urutkan_poin
-).grid(row=1, column=1, padx=5, pady=5)
 
-Button(frame_button, 
-       text="Tambah History",
-       width=18, 
-       command=tambah_history
-).grid(row=2, column=0, padx=5, pady=5)
+def tambah():
+    c = Club(
+        int(entry_id.get()),
+        entry_nama.get(),
+        entry_liga.get(),
+        int(entry_poin.get())
+    )
+    club_list.tambah(c)
+    refresh()
 
-Button(frame_button,
-       text="Lihat History",
-       width=18,
-       command=lihat_history
-).grid(row=2, column=1, padx=5, pady=5)
 
-Button(frame_button,
-       text="Tambah Watchlist",
-       width=18, 
-       command=tambah_watchlist
-).grid(row=2, column=2, padx=5, pady=5)
+def hapus():
+    club_list.hapus(int(entry_id.get()))
+    refresh()
 
-Button(frame_button,
-       text="Lihat Watchlist",
-       width=18, 
-       command=lihat_watchlist
-).grid(row=3, column=1, padx=5, pady=5)
 
-club1 = Club(1, "Real Madrid", "La Liga", 95)
-club2 = Club(2, "Manchester City", "Premier League", 97)
-club3 = Club(3, "Barcelona", "La Liga", 90)
-club4 = Club(4, "Liverpool", "Premier League", 88)
-club5 = Club(5, "Bayern Munich", "Bundesliga", 92)
+def update():
+    club_list.update(
+        int(entry_id.get()),
+        entry_nama.get(),
+        entry_liga.get(),
+        int(entry_poin.get())
+    )
+    refresh()
 
-club_list.tambah(club1)
-club_list.tambah(club2)
-club_list.tambah(club3)
-club_list.tambah(club4)
-club_list.tambah(club5)
 
-refresh_data()
+def cari():
+    hasil = cari_club(club_list.tampil_semua(), entry_nama.get())
 
+    listbox.delete(0, END)
+
+    for c in hasil:
+        listbox.insert(END, c.tampil())
+
+
+def top3():
+    data = bubble_sort(club_list.tampil_semua())[:3]
+
+    listbox.delete(0, END)
+    for c in data:
+        listbox.insert(END, c.tampil())
+
+
+frame = Frame(root, bg="#101820")
+frame.pack()
+
+Label(frame, text="ID").grid(row=0, column=0)
+entry_id = Entry(frame)
+entry_id.grid(row=0, column=1)
+
+Label(frame, text="Nama").grid(row=1, column=0)
+entry_nama = Entry(frame)
+entry_nama.grid(row=1, column=1)
+
+Label(frame, text="Liga").grid(row=2, column=0)
+entry_liga = Entry(frame)
+entry_liga.grid(row=2, column=1)
+
+Label(frame, text="Poin").grid(row=3, column=0)
+entry_poin = Entry(frame)
+entry_poin.grid(row=3, column=1)
+
+Button(frame, text="Tambah", command=tambah).grid(row=0, column=2)
+Button(frame, text="Update", command=update).grid(row=1, column=2)
+Button(frame, text="Hapus", command=hapus).grid(row=2, column=2)
+Button(frame, text="Cari", command=cari).grid(row=3, column=2)
+Button(frame, text="Top 3", command=top3).grid(row=4, column=2)
+
+club_list.tambah(Club(1, "Real Madrid", "La Liga", 95))
+club_list.tambah(Club(2, "Man City", "EPL", 97))
+club_list.tambah(Club(3, "Barcelona", "La Liga", 90))
+
+refresh()
 root.mainloop()

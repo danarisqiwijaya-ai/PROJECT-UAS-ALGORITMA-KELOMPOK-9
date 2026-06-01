be4d000f49e4444c7a14e08e521a1e1dc52f9bdf
danarisqiwijaya-ai <danarisqiwijaya@gmail.com>
2026-06-01 18:12:44 +0700

    Update Model.py

--- Model.py
+++ Model.py
@@ -6,7 +6,7 @@ class Club:
         self.poin = poin
 
     def tampil(self):
-        return f"{self.id} | {self.nama} | {self.liga} | {self.poin}"
+        return f"{self.id} | {self.nama} | {self.liga} | Poin: {self.poin}"
 
 
 class Node:
@@ -20,30 +20,65 @@ class LinkedList:
         self.head = None
 
     def tambah(self, data):
-        node = Node(data)
-        if not self.head:
-            self.head = node
-            return
-        cur = self.head
-        while cur.next:
-            cur = cur.next
-        cur.next = node
+        new_node = Node(data)
 
-    def tampil(self):
+        if self.head is None:
+            self.head = new_node
+            return
+
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
+
+    def tampil_semua(self):
         hasil = []
-        cur = self.head
-        while cur:
-            hasil.append(cur.data)
-            cur = cur.next
+        current = self.head
+
+        while current:
+            hasil.append(current.data)
+            current = current.next
+
         return hasil
+
+    def hapus(self, id_club):
+        current = self.head
+        prev = None
+
+        while current:
+            if current.data.id == id_club:
+                if prev:
+                    prev.next = current.next
+                else:
+                    self.head = current.next
+                return True
+
+            prev = current
+            current = current.next
+
+        return False
+
+    def update(self, id_club, nama, liga, poin):
+        current = self.head
+
+        while current:
+            if current.data.id == id_club:
+                current.data.nama = nama
+                current.data.liga = liga
+                current.data.poin = poin
+                return True
+
+            current = current.next
+
+        return False
 
 
 class Stack:
     def __init__(self):
         self.data = []
 
-    def push(self, x):
-        self.data.append(x)
+    def push(self, item):
+        self.data.append(item)
 
     def tampil(self):
         return self.data
@@ -53,8 +88,8 @@ class Queue:
     def __init__(self):
         self.data = []
 
-    def enqueue(self, x):
-        self.data.append(x)
+    def enqueue(self, item):
+        self.data.append(item)
 
     def tampil(self):
         return self.data

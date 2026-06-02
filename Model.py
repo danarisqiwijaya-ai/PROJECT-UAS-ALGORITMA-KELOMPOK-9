9febc35a5e0546e64b345ae7300182d8c3c02fb2
Muhammad Hibatullah <peshbt@gmail.com>
2026-06-02 17:44:51 +0700

    Update Model.py

--- Model.py
+++ Model.py
@@ -1,100 +1,26 @@
 class Club:
     def __init__(self, id_club, nama, liga, poin):
-        self.id = id_club
-        self.nama = nama
-        self.liga = liga
-        self.poin = poin
+        self.__id = id_club
+        self.__nama = nama
+        self.__liga = liga
+        self.__poin = poin
+
+    def get_id(self):
+        return self.__id
+
+    def get_nama(self):
+        return self.__nama
+
+    def get_liga(self):
+        return self.__liga
+
+    def get_poin(self):
+        return self.__poin
+
+    def set_data(self, nama, liga, poin):
+        self.__nama = nama
+        self.__liga = liga
+        self.__poin = poin
 
     def tampil(self):
-        return str(self.id) + " | " + self.nama + " | " + self.liga + " | " + str(self.poin)
-        from model import Club
-
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-
-
-class LinkedList:
-    def __init__(self):
-        self.head = None
-
-    def tambah(self, data):
-        new_node = Node(data)
-
-        if self.head is None:
-            self.head = new_node
-            return
-
-        current = self.head
-        while current.next:
-            current = current.next
-        current.next = new_node
-
-    def tampil_semua(self):
-        hasil = []
-        current = self.head
-
-        while current:
-            hasil.append(current.data)
-            current = current.next
-
-        return hasil
-
-    def hapus(self, id_club):
-        current = self.head
-        prev = None
-
-        while current:
-            if current.data.id == id_club:
-                if prev:
-                    prev.next = current.next
-                else:
-                    self.head = current.next
-                return True
-
-            prev = current
-            current = current.next
-
-        return False
-
-    def update(self, id_club, nama, liga, poin):
-        current = self.head
-
-        while current:
-            if current.data.id == id_club:
-                current.data.nama = nama
-                current.data.liga = liga
-                current.data.poin = poin
-                return True
-
-            current = current.next
-
-        return False
-
-
-class Stack:
-    def __init__(self):
-        self.data = []
-
-    def push(self, x):
-        self.data.append(x)
-
-    def tampil(self):
-        return self.data
-
-
-class Queue:
-    def __init__(self):
-        self.data = []
-
-    def enqueue(self, x):
-        self.data.append(x)
-
-    def tampil(self):
-        return self.data
-
-
-club_list = LinkedList()
-history_stack = Stack()
-watchlist_queue = Queue()
+        return f"{self.__id} | {self.__nama} | {self.__liga} | Poin: {self.__poin}")

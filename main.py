a3e7f5a4dc9ce4a71ce71ec465af62049ce6a307
SekarAyuMaheswari1 <ayusekar220507@gmail.com>
2026-06-02 07:13:15 +0700

    Update main.py

--- main.py
+++ main.py
@@ -59,34 +59,4 @@ def top3():
     listbox.delete(0, END)
     for c in data:
         listbox.insert(END, c.tampil())
-frame = Frame(root, bg="#101820")
-frame.pack()
 
-Label(frame, text="ID").grid(row=0, column=0)
-entry_id = Entry(frame)
-entry_id.grid(row=0, column=1)
-
-Label(frame, text="Nama").grid(row=1, column=0)
-entry_nama = Entry(frame)
-entry_nama.grid(row=1, column=1)
-
-Label(frame, text="Liga").grid(row=2, column=0)
-entry_liga = Entry(frame)
-entry_liga.grid(row=2, column=1)
-
-Label(frame, text="Poin").grid(row=3, column=0)
-entry_poin = Entry(frame)
-entry_poin.grid(row=3, column=1)
-
-Button(frame, text="Tambah", command=tambah).grid(row=0, column=2)
-Button(frame, text="Update", command=update).grid(row=1, column=2)
-Button(frame, text="Hapus", command=hapus).grid(row=2, column=2)
-Button(frame, text="Cari", command=cari).grid(row=3, column=2)
-Button(frame, text="Top 3", command=top3).grid(row=4, column=2)
-
-club_list.tambah(Club(1, "Real Madrid", "La Liga", 95))
-club_list.tambah(Club(2, "Man City", "EPL", 97))
-club_list.tambah(Club(3, "Barcelona", "La Liga", 90))
-
-refresh()
-root.mainloop()

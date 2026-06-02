6c327fac8f75bef5540303d52eb1380d1f77aa9e
gitamarbun23-design <gitamarbun23@gmail.com>
2026-06-02 15:19:16 +0700

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,21 +1,32 @@
-def cari_club(data, keyword):
-    return [c for c in data if keyword.lower() in c.nama.lower()]
+def cari_club(data, key):
+    return [c for c in data if key.lower() in c.nama.lower()]
 
 
-def bubble_sort_poin(data):
+def bubble_sort(data):
     n = len(data)
+
     for i in range(n):
         for j in range(n - i - 1):
             if data[j].poin < data[j + 1].poin:
                 data[j], data[j + 1] = data[j + 1], data[j]
+
     return data
 
 
-def rekomendasi_top(data, top_n=3):
-    data = bubble_sort_poin(data)
-    return data[:top_n]
+def binary_search(data, key):
+    data = sorted(data, key=lambda x: x.nama.lower())
 
+    l, r = 0, len(data) - 1
 
-def rekomendasi_liga(data, liga):
-    filtered = [c for c in data if c.liga.lower() == liga.lower()]
-    return bubble_sort_poin(filtered)
+    while l <= r:
+        m = (l + r) // 2
+
+        if key.lower() in data[m].nama.lower():
+            return data[m]
+
+        elif key.lower() < data[m].nama.lower():
+            r = m - 1
+        else:
+            l = m + 1
+
+    return None

bd911c8fc8ab13711f478b1592be27ca4e6d8fc8
winatapandu <winatapandu623@gmail.com>
2026-06-03 13:11:23 +0700

    Replace Club class with Handphone class

--- Model.py
+++ Model.py
@@ -1,26 +1,11 @@
-class Club:
-    def __init__(self, id_club, nama, liga, poin):
-        self.__id = id_club
-        self.__nama = nama
-        self.__liga = liga
-        self.__poin = poin
+class Handphone:
+    def __init__(self, id_hp, nama, harga, ram, storage, rating):
+        self.id = id_hp
+        self.nama = nama
+        self.harga = harga        
+        self.ram = ram            
+        self.storage = storage
+        self.rating = rating     
 
-    def get_id(self):
-        return self.__id
-
-    def get_nama(self):
-        return self.__nama
-
-    def get_liga(self):
-        return self.__liga
-
-    def get_poin(self):
-        return self.__poin
-
-    def set_data(self, nama, liga, poin):
-        self.__nama = nama
-        self.__liga = liga
-        self.__poin = poin
-
-    def tampil(self):
-        return f"{self.__id} | {self.__nama} | {self.__liga} | Poin: {self.__poin}")
+    def __str__(self):
+        return f"{self.nama} (RAM: {self.ram}GB, Storage: {self.storage}GB) - Rp{self.harga:,} [Rating: {self.rating}]"

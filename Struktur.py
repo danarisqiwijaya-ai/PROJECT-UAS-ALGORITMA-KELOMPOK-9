4eb97c61e5b8c33d26658ceb30c18e240d23f59c
gitamarbun23-design <gitamarbun23@gmail.com>
2026-06-03 21:27:27 +0700

    Update Struktur.py

--- Struktur.py
+++ Struktur.py
@@ -1,4 +1,4 @@
-class Node:
+class DLLNode:
     def __init__(self, data):
         self.data = data  
         self.next = None  
@@ -11,7 +11,7 @@ class HistoryLinkedList:
         self.current = None  
 
     def add_history(self, hp_object):
-        new_node = Node(hp_object)
+        new_node = DLLNode(hp_object)
         if not self.head:
             self.head = new_node
             self.tail = new_node
@@ -32,3 +32,62 @@ class HistoryLinkedList:
             self.current = self.current.next
             return self.current.data
         return None
+
+class ComparisonQueue:
+    def __init__(self):
+        self.items = []
+
+    def enqueue(self, item):
+        if len(self.items) >= 3:
+            return False
+        self.items.append(item)
+        return True
+
+    def dequeue(self):
+        if not self.is_empty():
+            return self.items.pop(0)
+        return None
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def get_all(self):
+        return self.items
+
+class BSTNode:
+    def __init__(self, hp_object):
+        self.hp = hp_object
+        self.left = None
+        self.right = None
+
+class HandphoneBST:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, hp_object):
+        if not self.root:
+            self.root = BSTNode(hp_object)
+        else:
+            self._insert_recursive(self.root, hp_object)
+
+    def _insert_recursive(self, node, hp_object):
+        if hp_object.rating < node.hp.rating:
+            if not node.left:
+                node.left = BSTNode(hp_object)
+            else:
+                self._insert_recursive(node.left, hp_object)
+        else:
+            if not node.right:
+                node.right = BSTNode(hp_object)
+            else:
+                self._insert_recursive(node.right, hp_object)
+
+    def search_exact_rating(self, rating_target):
+        return self._search_recursive(self.root, rating_target)
+
+    def _search_recursive(self, node, rating_target):
+        if not node or node.hp.rating == rating_target:
+            return node.hp if node else None
+        if rating_target < node.hp.rating:
+            return self._search_recursive(node.left, rating_target)
+        return self._search_recursive(node.right, rating_target)
